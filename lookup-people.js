259fac026e3448ae8d7bb223ff0f8b599fbb371b
Kelly James <kellyjamesclancy@gmail.com>
2016-11-22 21:47:57 +0000

    People finder working properly

--- lookup-people.js
+++ lookup-people.js
@@ -1,7 +1,8 @@
 const pg       = require('pg');
 const settings = require('./settings');
+const input    = process.argv[2];
 
-const client   = new pg.Client ({
+const client   = new pg.Client({
   user         : settings.user,
   password     : settings.password,
   database     : settings.database,
@@ -11,10 +12,12 @@ const client   = new pg.Client ({
 });
 
 client.connect((err) => {
+  console.log('Searching...');
+  const sql = "SELECT * FROM famous_people WHERE famous_people.last_name = $1;"
   if(err) {
     return console.error('Connection Error', err);
   }
-  client.query("select * from fleets where fleets.name = $1;", [''], (err, result) => {
+  client.query(sql, [`${input}`], (err, result) => {
     if(err) {
       return console.error('Error Running Query', err);
     }
